79afa534c999675c2e61722a34d09b0f74526347
linwj <1745727332@qq.com>
2022-05-25 09:58:56 +0800

    practive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostModule } from './modules/post/post.module';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PageNotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, PostModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/post/post.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostComponent } from './post.component';
+
+import { PostRoutingModule } from './post-routing.module';
+
+@NgModule({
+  declarations: [PostComponent],
+  imports: [CommonModule, PostRoutingModule],
+  exports: [PostComponent],
+})
+export class PostModule {}
